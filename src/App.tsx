7ba74d2da818881c965cc51e722ed1f8e92b4bee
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-11-24 21:45:22 -0800

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,9 @@
 import './App.css'
 
-let name: string = 'Mikyle'
-let age: number | string
-
-let printName: (name: string) => void
-
-let personName: unknown
-
-let printName2: (name: string) => never
-
-let isStudent: boolean
-let hobbies: number[]
-let role: [number, string]
-
-// type Person = {
-//   name: string
-//   age?: number
-// }
-
-// let person: Person = {
-//   name: 'Mikyle',
-// }
-
-// let lotsOfPeople: Person[]
-
-type X = {
-  a: string
-  b: number
-}
-
-type Y = X & {
-  c: string
-  d: number
-}
-
-let y: Y = {
-  a: 'hello',
-  b: 2,
-  c: 'efdats',
-  d: 42,
-}
-
-interface Person {
-  name: string
-  age?: number
-}
-
-interface Guy extends Person {
-  profession: string
-}
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className='App'>
-      <p>Hello World</p>
+      <span className='heading'>Taskify</span>
     </div>
   )
 }
