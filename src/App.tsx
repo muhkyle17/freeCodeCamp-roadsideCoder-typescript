663c4814df4d6d2ed40bedd04eae4f2600cce68d
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-11-24 21:47:16 -0800

    Init input field component in the components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import InputField from './components/InputField'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
+      <InputField />
     </div>
   )
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <div>
+      <p>InputField</p>
+    </div>
+  )
+}
+
+export default InputField
